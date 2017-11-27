d79b0b1477238f63e3b7f96f1e84816f7879784f
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-27 23:41:00 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,11 @@
-def my_select(&thang)
-arr = []
-   self.my_each do |x|
-     arr << thang.call(x)
+def my_select(array)
+  i = 0
+  select_elements = []
+  while i < array.length
+    if yield(array[i])
+      select_elements << array[i]
    end
-   arr.compact
- end
+        i+=1
+  end
+  select_elements
+end
